a4a201b8eb9b507e6085633a9f72da9db377c4c4
Caeron Seabourne <caeking93@gmail.com>
2018-05-14 13:39:07 +0100

    Completed the Async Initial entries suite

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -83,15 +83,26 @@ $(function() {
     /* TODO: Write a new test suite named "Initial Entries" */
     describe('Initial Entires', function() {
 
+      beforeEach(function(done) {
+        loadFeed(0,function() {
+          done();
+        });
+      });
+
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
-         
+         it('there is at least one feed in the RSS feed', function(done) {
+           const container = $('.feed');
+           expect($(container).html()).not.toBe('');
+           done();
+         });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
+    
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
